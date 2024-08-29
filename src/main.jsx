7c6be4bd339ccd3db443bd01046e91d7f2d263c5
ssm5158 <shubha.ssm@gmail.com>
2024-08-29 18:50:57 +0530

    0829 650

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,10 @@ import { Provider } from "react-redux";
 import store from "./store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter basename="/e-plantShopping/">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
